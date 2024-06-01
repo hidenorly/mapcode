0a8fad4193e5319d6c78cefb633c0e1a172a9da8
hidenorly <twitte.harold@gmail.com>
2024-06-02 00:38:40 +0900

    Add Copyright

--- get_mapcode.py
+++ get_mapcode.py
@@ -1,18 +1,30 @@
+#   Copyright 2024 hidenorly
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+
 import requests
 import sys
 
 def get_mapcode(latitude, longitude):
-    # ポストするデータを準備
     data = {
         "t": "jpndeg",
         "jpn_lat": str(latitude),
         "jpn_lon": str(longitude)
     }
-    
-    # リクエストを送信
+
+    # Thank you so much for hosting this web service
     response = requests.post("https://saibara.sakura.ne.jp/map/convgeo.cgi", data=data)
-    
-    # レスポンスからMapcodeを抽出
     mapcode = response.text.split('name="mapcode" value="')[1].split('"')[0]
     
     return mapcode
